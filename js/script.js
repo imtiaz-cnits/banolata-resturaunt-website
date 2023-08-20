ffa38fd035e58ceab98b34039724d205df73ba20
rahatcnits <rahatcnits@gmail.com>
2023-08-20 18:40:32 +0600

    footer section work

--- js/script.js
+++ js/script.js
@@ -25,3 +25,25 @@ tabBtn.forEach((tab, index) => {
     tabContent[index].classList.add("active");
   });
 });
+
+// back to top btn
+
+//Get the button:
+mybutton = document.getElementById("bt-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 1000 ||
+    document.documentElement.scrollTop > 1000
+  ) {
+    mybutton.style.display = "block";
+    mybutton.style.zIndex = 100;
+  } else {
+    mybutton.style.display = "none";
+  }
+}
